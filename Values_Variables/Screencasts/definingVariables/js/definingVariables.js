d45ac300192282441550bb47e728df3bf2c3b2ff
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-07 22:47:26 -0500

    Screencast: Declaring & Defining Variables
    Screencast: Declaring & Defining Variables;  created html5 and js files

---
+++ Values_Variables/Screencasts/definingVariables/js/definingVariables.js
@@ -0,0 +1,13 @@
+/**Values and Variables
+ * Screencast: Defining Variables
+ * Lucas Gabbart
+ * 5/7/15.
+ */
+
+//alert("js works");
+
+var age; //declaration
+age = 38; //definition
+//storage container = value
+
+var yearOfBirth = 1970; //declaring and defining on the same line
