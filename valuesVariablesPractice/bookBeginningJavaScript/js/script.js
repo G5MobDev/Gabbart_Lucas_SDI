af418ce00eab39a0f570504f4ab36cc3b168eed7
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-07 20:36:53 -0500

    created ch2_examp1.htm file
    created ch2_examp1.htm file for Beginning JavaScript book practice
    example

---
+++ valuesVariablesPractice/bookBeginningJavaScript/js/script.js
@@ -0,0 +1,5 @@
+/**
+ * Created by lucasgabbart on 5/7/15.
+ */
+
+//alert("Book: Beginning JavaScript\nChapter 2: Pages 17 to 26");
--- valuesVariablesPractice/js/script.js
+++ valuesVariablesPractice/js/script.js
@@ -1,3 +1,5 @@
 /**
  * Created by lucasgabbart on 5/7/15.
  */
+
+alert("This is in the js file!");
