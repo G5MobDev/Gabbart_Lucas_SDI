e91ed05147674b16d7e870d73baf7070053d1688
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-07 22:24:27 -0500

    updated console.log() & alert() js file
    updated console.log() & alert() js files in preparation for
    following screencast

--- Values_Variables/Screencasts/consoleLog_alert/js/consoleLog_alert.js
+++ Values_Variables/Screencasts/consoleLog_alert/js/consoleLog_alert.js
@@ -1,5 +1,7 @@
-/**
- * Created by lucasgabbart on 5/7/15.
+/**Values and Variables
+ * Screencast: console.log() and alert()
+ * Lucas Gabbart
+ * 5/7/15.
  */
 
-alert("js is working!");
+//alert("js is working!");
