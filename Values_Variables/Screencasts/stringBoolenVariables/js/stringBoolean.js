8815191e974666b52534b277cd0804555f0f6335
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-09 20:27:03 -0500

    created practice js for Screencast: Strings & Booleans
    created practice js for Screencast: Strings & Boolean Variables

--- Values_Variables/Screencasts/stringBoolenVariables/js/stringBoolean.js
+++ Values_Variables/Screencasts/stringBoolenVariables/js/stringBoolean.js
@@ -4,4 +4,15 @@
  * 5/9/15.
  */
 
-alert("js works");
+var age = 38; // Number variable
+
+var isStudent = true;//or false
+
+var firstName = "Lucas";//string variable(encased in quotes)
+
+var phrase = "I don\'t know \n You\'ll have to speak with the professor";
+
+console.log(age);
+console.log(isStudent);
+console.log(firstName);
+console.log(phrase);
