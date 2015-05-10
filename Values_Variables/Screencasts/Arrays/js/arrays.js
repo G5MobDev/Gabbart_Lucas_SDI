25b055105e591b4dd4d8ff50a694bb370d77773e
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-09 20:39:25 -0500

    practice Screencast: Arrays
    practice Screencast: Arrays

--- Values_Variables/Screencasts/Arrays/js/arrays.js
+++ Values_Variables/Screencasts/Arrays/js/arrays.js
@@ -4,4 +4,13 @@
  * 5/9/15.
  */
 
-alert("js works");//tested link between html and js files
+//alert("js works");//tested link between html and js files
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];//0,1,2,3(order of variable positions for calling
+
+muppetNames[1] = "Rizzo";
+muppetNames[2] = "Fozzy";
+
+var num = 2;
+
+console.log(muppetNames[num]);
