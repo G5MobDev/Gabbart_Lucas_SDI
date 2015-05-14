0dedcba29184eac1bceaad997b6abf287b11db90
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 17:15:27 -0500

    created elseIf directory
    created elseIf directory with html5 and js files

---
+++ Week_3_Practice: Conditionals/elseIf/js/elseIf.js
@@ -0,0 +1,23 @@
+/**Screencast: Else If
+ * Lucas Gabbart
+ * 5/14/15.
+ */
+
+//alert("else if");
+
+var kidHeight = 45;
+var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+	//you can ride!
+	console.log("You can ride the coaster!");
+}else if(kidHeight >= wParentHeight){
+	//you can ride with a parent present
+	console.log("You can ride, but only with a parent present.");
+}else {
+	//sorry you have growing to do
+	console.log("Sorry kid, you've got some growing to do first!");
+}
