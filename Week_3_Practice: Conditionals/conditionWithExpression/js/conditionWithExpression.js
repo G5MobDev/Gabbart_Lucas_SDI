3de33b7b70f4d11b7e45a7a0f83b0213f1b1c242
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 16:51:54 -0500

    created conditionWithExpression directory
    created conditionWithExpression directory with html5 and js files

---
+++ Week_3_Practice: Conditionals/conditionWithExpression/js/conditionWithExpression.js
@@ -0,0 +1,17 @@
+/**Screencast: Condition With Expression
+ * Lucas Gabbart
+ * 5/14/15.
+ */
+
+//alert("Condition with Expression");
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
