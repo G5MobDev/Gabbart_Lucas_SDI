eb24b1d6014b8dbe0ddb6b16954bb3b26e909f9c
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-13 20:10:06 -0500

    added dogYears sub-directory to expressions worksheet
    added dogYears sub-directory to expressions worksheet directory

---
+++ Expressions_worksheet/Dog_Years/js/dogYears.js
@@ -0,0 +1,15 @@
+/**Expressions Worksheet
+ * Dog Years
+ * Lucas Gabbart
+ * 5/13/15.
+ */
+
+alert("Dog Years");
+
+var dogYears;
+var humanYears = prompt("We are calculating Sparky\'s age.\nPlease enter his age in human years.");//variable used to compute age in dog years
+
+dogYears = humanYears * 7;
+
+alert("Sparky is " + humanYears + " human years old, which is " + dogYears + " in dog years.");
+console.log("Sparky is " + humanYears + " years old, which is " + dogYears + " in dog years.");
