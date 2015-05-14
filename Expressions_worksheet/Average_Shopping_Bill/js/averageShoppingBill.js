23367e5ab0e1aa5575038c334e81ed9eb194d255
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-13 21:43:17 -0500

    Expressions Worksheet: Average Shopping Bill
    Expressions Worksheet: Average Shopping Bill

--- Expressions_worksheet/Average_Shopping_Bill/js/averageShoppingBill.js
+++ Expressions_worksheet/Average_Shopping_Bill/js/averageShoppingBill.js
@@ -6,3 +6,8 @@
 
 alert("Average Shopping Bill");
 
+var groCost = [246, 275, 234, 298, 317];
+var groTotal = groCost[0]+groCost[1]+groCost[2]+groCost[3]+groCost[4];
+var weekAverage = groTotal/5;
+
+console.log("You have spent a total of $" + groTotal + " on groceries over 5 weeks.  That is an average of $" + weekAverage + " per week.");
