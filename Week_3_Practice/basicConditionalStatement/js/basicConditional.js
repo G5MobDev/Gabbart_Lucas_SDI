ce6fa0c092691c2d10fca004476b1bbfb5cc351f
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 16:39:58 -0500

    created basicConditionalStatement directory
    created basicConditionalStatement directory with html5 and js files.
    followed screencast

---
+++ Week_3_Practice/basicConditionalStatement/js/basicConditional.js
@@ -0,0 +1,17 @@
+/**Screencast: Basic Conditional Statement
+ * Lucas Gabbart
+ * 5/14/15.
+ */
+
+//alert("Basic Conditional Statement");
+
+var oldEnough = true;
+//var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+//console.log("what comes after");
+
