34a43a4095d8035e58af210337d8408ac3895276
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-13 21:12:52 -0500

    created Slice of Pie: Part 2 directory
    created Slice of Pie: Part 2 directory with html5 and js files

---
+++ Expressions_worksheet/Slice_of_Pie_2/js/slicePie2.js
@@ -0,0 +1,17 @@
+/**Expressions Worksheet
+ * Slice of Pie Part 2
+ * Lucas Gabbart
+ * 5/13/15.
+ */
+
+alert("Slice of Pie: Part 2");
+
+var perPizza = 10; //number of slices per pizza
+var partyGoers = 25; //number of people at the party
+var pizzas = 13; //number of pizzas that were ordered
+var slices; //number of slices of pizza that each partygoer gets
+
+slices = (pizzas*perPizza)/partyGoers;
+
+alert("Each person ate " + slices + " slices of pizza at the party.");
+console.log("Each person ate " + slices + " slices of pizza at the party.");
