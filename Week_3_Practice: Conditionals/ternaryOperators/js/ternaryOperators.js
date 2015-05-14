3b587e1352b6be71496442e8c5742660eabfebb0
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 17:52:11 -0500

    Screencast: Ternary Operators
    followed Screencast: Ternary Operators

--- Week_3_Practice: Conditionals/ternaryOperators/js/ternaryOperators.js
+++ Week_3_Practice: Conditionals/ternaryOperators/js/ternaryOperators.js
@@ -3,4 +3,11 @@
  * 5/14/15.
  */
 
-alert("Ternary Operators");
+//alert("Ternary Operators");
+
+var age = 16;
+var book;
+
+//If the child is under 10, they get Green Eggs and Ham, otherwise the get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
