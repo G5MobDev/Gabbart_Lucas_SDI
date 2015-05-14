1122ad0c78a8d8bc8c6571c10b0daab93f9c075d
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-13 21:10:15 -0500

    Expressions Worksheet: Slices of Pie: Part 1
    Expressions Worksheet: Slices of Pie: Part 1

--- Expressions_worksheet/Slice_of_Pie_1/js/slicePie1.js
+++ Expressions_worksheet/Slice_of_Pie_1/js/slicePie1.js
@@ -6,3 +6,12 @@
 
 alert("Slice of Pie: Part 1");
 
+var perPizza = 10; //number of slices per pizza
+var partyGoers = 25; //number of people at the party
+var pizzas = 13; //number of pizzas that were ordered
+var slices; //number of slices of pizza that each partygoer gets
+
+slices = (pizzas*perPizza)/partyGoers;
+
+alert("Each person ate " + slices + " slices of pizza at the party.");
+console.log("Each person ate " + slices + " slices of pizza at the party.");
