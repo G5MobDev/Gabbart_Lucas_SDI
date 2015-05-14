bf6b8bff9804283ab16bc132973c964a4d1a8486
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 01:52:09 -0500

    Expressions Worksheet: Discounts
    Expressions Worksheet: Discounts

--- Expressions_worksheet/Discounts/js/discounts.js
+++ Expressions_worksheet/Discounts/js/discounts.js
@@ -4,4 +4,16 @@
  * 5/14/15.
  */
 
-alert("Discounts");
+//alert("Discounts");
+
+var item = prompt("Enter a description of your item.");
+var originPrice = prompt("Enter the price of your item.");
+var discount = 15/100;
+var tax = 9.5/100;
+originPrice = Number(originPrice);
+var noTax = originPrice-(discount*originPrice);
+var withTax = noTax+(noTax*tax);
+noTax = noTax.toFixed(2);
+withTax = withTax.toFixed(2);
+
+console.log("Your "+item+" was originally $"+Number(originPrice)+", but after a $"+discount+"% discount, it is now $"+noTax+" without tax, and $"+withTax+" with tax.");
