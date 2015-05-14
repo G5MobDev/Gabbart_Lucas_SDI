3c0dfaa316d2b6a204f1495917b6fc4ab30e73f4
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 01:59:25 -0500

    Expressions Worksheet: Discounts edit
    Expressions Worksheet: Discounts edit for prompts

--- Expressions_worksheet/Discounts/js/discounts.js
+++ Expressions_worksheet/Discounts/js/discounts.js
@@ -8,10 +8,12 @@
 
 var item = prompt("Enter a description of your item.");
 var originPrice = prompt("Enter the price of your item.");
-var discount = 15/100;
-var tax = 9.5/100;
+var discount = prompt("Enter the discount percentage.\n(Example: 20% = 20)");
+var tax = prompt("Enter the sales tax percentage.\n(Example: 10% = 10)");
 originPrice = Number(originPrice);
-var noTax = originPrice-(discount*originPrice);
+discount = Number(discount);
+tax = Number(tax)/100;
+var noTax = originPrice-(discount/100*originPrice);
 var withTax = noTax+(noTax*tax);
 noTax = noTax.toFixed(2);
 withTax = withTax.toFixed(2);
