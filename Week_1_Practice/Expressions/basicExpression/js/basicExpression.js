51c612ac72eb9503a8abc2b29a3fff81e7e6219c
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-13 20:06:00 -0500

    basic expression practice

---
+++ Week_1_Practice/Expressions/basicExpression/js/basicExpression.js
@@ -0,0 +1,12 @@
+/**Screencast
+ * Basic Expression
+ *Lucas Gabbart
+ *5/13/15.
+ */
+
+//alert("js works");
+
+var a = 2; //sets up and defines variable with 2
+var b;
+b = a + 3; //add 3 to a
+console.log(b);
