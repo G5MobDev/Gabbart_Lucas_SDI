a804fc62abca72e81cea4e7414247e748b061e24
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 16:47:30 -0500

    created relationalExpressions directory
    created relationalExpressions directory with html5 and js files

---
+++ Week_3_Practice: Conditionals/relationalExpressions/js/relationalExpressions.js
@@ -0,0 +1,16 @@
+/**Screencast: Relational Expressions
+ * Lucas Gabbart
+ * 5/14/15.
+ */
+
+//alert("Relational Expressions");
+
+var kidHeight = 49;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
