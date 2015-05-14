c95099ce42a5c819cb88d9237a28a2050dff0055
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 17:47:46 -0500

    Screencast: Logical Operators
    followed Screencast: Logical Operators

--- Week_3_Practice: Conditionals/logicalOperators/js/logicalOperators.js
+++ Week_3_Practice: Conditionals/logicalOperators/js/logicalOperators.js
@@ -5,3 +5,13 @@
 
 //alert("Logical Operators");
 
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of the phone is less our budget or we won the lottery
+if(iPhonePrice < budget || wonLottery){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you!!");
+}
