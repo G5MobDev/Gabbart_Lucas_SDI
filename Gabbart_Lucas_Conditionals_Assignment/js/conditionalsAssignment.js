924ffc67f09c3f5305d1714714c20558f43429af
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-14 21:30:09 -0500

    Gabbart_Lucas_Conditionals_Assignment
    created Gabbart_Lucas_Conditionals_Assignment directory with
    conditionalsAssignment.html/.js files.
    Checked to ensure .js is linked to .html

---
+++ Gabbart_Lucas_Conditionals_Assignment/js/conditionalsAssignment.js
@@ -0,0 +1,10 @@
+/**Conditionals Assignment
+ * Lucas Gabbart
+ * 5/14/15
+ * Course: MDV2330-O
+ * Term C201505
+ * Section 02
+ * Instructor: Dan Williams
+ */
+
+//alert("Conditionals Assignment");
