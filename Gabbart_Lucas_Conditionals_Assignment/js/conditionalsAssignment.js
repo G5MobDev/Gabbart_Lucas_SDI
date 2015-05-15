9e8a966f03e484462dbe6eac4dbf7da709e56756
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-15 15:13:17 -0500

    continued work on conditionals assignment
    continued work on conditionals assignment

--- Gabbart_Lucas_Conditionals_Assignment/js/conditionalsAssignment.js
+++ Gabbart_Lucas_Conditionals_Assignment/js/conditionalsAssignment.js
@@ -10,22 +10,21 @@
 
 //alert("Conditionals Assignment");
 
-var surface,//user input variable that provides the pressure of the well at the surface for use in calculating bottom hole pressure
-	hydrostatic,//variable that will be calculated from surface and tvd variables
-	tvd,//user input variable that provides the vertical depth of the well for use in calculating bottom hole pressure
-	fluid,//user input variable that provides the type of fluid in the well to determine the proper gradient for use in calculating bottom hole pressure
-	bhp;//bottom hole pressure variable that will be calculated from surface, hydrostatic, and fluid variables and will provide the final result that will print to console.log()
+var surface;//user input variable that provides the pressure of the well at the surface for use in calculating bottom hole pressure
+var hydrostatic;//variable that will be calculated from surface and tvd variables
+var tvd;//user input variable that provides the vertical depth of the well for use in calculating bottom hole pressure
+var fluid;//user input variable that provides the type of fluid in the well to determine the proper gradient for use in calculating bottom hole pressure
+var bhp;//bottom hole pressure variable that will be calculated from surface, hydrostatic, and fluid variables and will provide the final result that will print to console.log()
 var water = 0.465;//defined variable that will provide the gradient of water for the bhp calculation
 var oil = 0.350;//defined variable that will provide the gradient of oil for the bhp calculation
 var gas = 0.100;//defined variable that will provide the gradient of gas for the bhp calculation
-
 
 surface = prompt("Enter the surface pressure of the well.");
 tvd = prompt("Enter the True Vertical Depth of the well.");
 fluid = prompt("Enter the type of liquid in the well.\n(water, oil, or gas)");
 
 if(!surface){
-	surface = prompt ( "You must enter a value for the surface pressure of the well." );
+	surface = prompt("You must enter a value for the surface pressure of the well.");
 }
 if(!tvd){
 	tvd = prompt("You must enter the True Vertical Depth of the well.");
@@ -34,13 +33,19 @@ if(!fluid){
 	fluid = prompt("You must enter the type of fluid in the well bore.");
 }
 
-if(fluid == "water"){
+surface = Number(surface);
+tvd = Number(tvd);
+
+console.log(surface);
+console.log(tvd);
+console.log(fluid);
+
+/*if(fluid === "water"){
 	hydrostatic = tvd * water;
-}else if(fluid == "oil"){
-	hydrostatic = tvd * oil;
-}else{
-	hydrostatic = tvd * gas;
 }
+(fluid == "oil" && fluid != "water") ? hydrostatic = tvd * oil : hydrostatic = tvd * gas;
+*/
+
 
 
 
