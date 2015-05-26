7b5b8ae2133a4c96ae3cb3186701c7dbb5b2a254
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 22:56:26 -0500

    practice anonymousFunctions
    practice anonymousFunctions

--- Week_4_Practice: Functions/anonymousFunctions/js/anonymousFunctions.js
+++ Week_4_Practice: Functions/anonymousFunctions/js/anonymousFunctions.js
@@ -2,4 +2,16 @@
  * Lucas Gabbart
  * 5/25/15.
  */
-alert("Anonymous Functions");
+//alert("Anonymous Functions");
+
+
+//anonymous functions MUST be declared BEFORE they're called
+var calcAreaF = function(width, height){
+	//code the function runs
+	var area = width * height;
+	return area;//returns area value to variable "a"
+}
+
+var a = calcAreaF(20, 30);//invoking the function
+
+console.log(a);
