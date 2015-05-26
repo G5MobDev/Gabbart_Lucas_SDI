a6562be4a1acd8e02d0b118941112508c02ed654
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 23:26:21 -0500

    practice forLoops
    practice forLoops

--- Week_4_Practice: Functions/forLoop/js/forLoop.js
+++ Week_4_Practice: Functions/forLoop/js/forLoop.js
@@ -2,4 +2,29 @@
  * Lucas Gabbart
  * 5/25/15.
  */
-alert("for Loop");
+//alert("for Loop");
+
+/*var b = 5;//sets up the index
+
+while (b > 0){//checks the condition
+	console.log(b + " kegs on the wall");
+	b--;//decrements "b" by 1 each time; b++ increments by 1 each time
+}
+
+console.log("-------- Do While Loops --------");
+
+var c = 10;
+
+do{
+	//always runs this code first
+	console.log(c + " kegs on the wall");
+	c--;
+}
+while(c > 0);//checks condition of code inside "do" until value is met
+
+console.log("-------- For Loops --------");
+*/
+
+for(var i = 10; i > 0; i--){//combines "while" & "do-while" loops
+	console.log(i + " kegs on the wall");
+}
