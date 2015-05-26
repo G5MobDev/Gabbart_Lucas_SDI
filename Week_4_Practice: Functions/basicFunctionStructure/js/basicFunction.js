e12f30a3b1ba0371705c3e8d15d0d4d06cf0a794
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 20:29:22 -0500

    practice basicFunctionStructure
    practice basicFunctionStructure from Screencast

--- Week_4_Practice: Functions/basicFunctionStructure/js/basicFunction.js
+++ Week_4_Practice: Functions/basicFunctionStructure/js/basicFunction.js
@@ -3,4 +3,19 @@
  * 5/25/15.
  */
 
-alert("Basic Function Structure");
+//alert("Basic Function Structure");
+
+/*function outptMsg(){
+	console.log("Hello World");
+}*/
+
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	return area;
+}
+var area = calcArea();
+
+console.log(area);
