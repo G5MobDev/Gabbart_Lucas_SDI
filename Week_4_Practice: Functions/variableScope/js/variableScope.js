f1ae3c327421cb8300d13711eb6cb0a19e75d685
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 21:09:38 -0500

    practice variableScope
    practice variableScope

--- Week_4_Practice: Functions/variableScope/js/variableScope.js
+++ Week_4_Practice: Functions/variableScope/js/variableScope.js
@@ -3,4 +3,15 @@
  * 5/25/15.
  */
 
-alert("Variable Scope");
+//alert("Variable Scope");
+
+var width = 5;
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	//console.log(area);
+}
+console.log(width);
+calcArea();
