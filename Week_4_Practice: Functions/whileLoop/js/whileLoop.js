3f7e5515fea24be455935c35fc3141415494a35b
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 23:13:16 -0500

    practice whileLoop
    practice whileLoop

--- Week_4_Practice: Functions/whileLoop/js/whileLoop.js
+++ Week_4_Practice: Functions/whileLoop/js/whileLoop.js
@@ -2,4 +2,11 @@
  * Lucas Gabbart
  * 5/25/15.
  */
-alert("while Loop");
+//alert("while Loop");
+
+var b = 50;//sets up the index
+
+while (b > 0){//checks the condition
+	console.log(b + " kegs on the wall");
+	b--;//decrements "b" by 1 each time; b++ increments by 1 each time
+}
