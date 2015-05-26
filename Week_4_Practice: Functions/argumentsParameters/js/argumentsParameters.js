b687de58639c0fd089ffa838e24455944362ca35
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 22:24:22 -0500

    practice argumentsParameters
    practice argumentsParameters from screencast

--- Week_4_Practice: Functions/argumentsParameters/js/argumentsParameters.js
+++ Week_4_Practice: Functions/argumentsParameters/js/argumentsParameters.js
@@ -3,4 +3,19 @@
  * 5/25/15.
  */
 
-alert("Arguments & Parameters");
+//alert("Arguments & Parameters");
+
+/*calcArea(30, 20);
+
+function calcArea(w, h){ //w=30, h=20
+	var area = w * h;
+	console.log(area);
+}*/
+
+function dogYears(age){//age is a parameter
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.");
+}
+var age1 = 4;
+dogYears(age1);//age1 is an argument
+dogYears(5);//5 is an argument
