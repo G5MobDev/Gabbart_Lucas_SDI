bccf64721eb4c68e24f2b370b78158c663b099a7
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 22:32:44 -0500

    practice returningValues
    practice returningValues from screencast

--- Week_4_Practice: Functions/returningValues/js/returningValues.js
+++ Week_4_Practice: Functions/returningValues/js/returningValues.js
@@ -2,5 +2,13 @@
  * Lucas Gabbart
  * 5/25/15.
  */
+//alert("Returning Values");
 
-alert("Returning Values");
+var total = calcArea(30, 20);//function returns info to here
+
+function calcArea(w, h){ //w=30, h=20
+	var area = w * h;
+	return area;//spitting info out
+}
+
+console.log(total);
