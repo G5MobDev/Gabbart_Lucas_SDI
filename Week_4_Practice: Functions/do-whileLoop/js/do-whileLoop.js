e56d322fbf8136f90fba6420938368429b8a5844
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 23:20:03 -0500

    practice do-whileLoop
    practice do-whileLoop

--- Week_4_Practice: Functions/do-whileLoop/js/do-whileLoop.js
+++ Week_4_Practice: Functions/do-whileLoop/js/do-whileLoop.js
@@ -4,3 +4,20 @@
  */
 //alert("do-while Loop");
 
+var b = 5;//sets up the index
+
+while (b > 0){//checks the condition
+	console.log(b + " kegs on the wall");
+	b--;//decrements "b" by 1 each time; b++ increments by 1 each time
+}
+
+console.log("-------- Do While Loops --------");
+
+var c = 10;
+
+do{
+	//always runs this code first
+	console.log(c + " kegs on the wall");
+	c--;
+}
+while(c > 0);//checks condition of code inside "do" until value is met
