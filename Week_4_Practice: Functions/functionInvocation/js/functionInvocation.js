cc10c98782d765a6060f658dfeb2bb28329fd5de
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-25 20:59:04 -0500

    practice functionInvocation
    practice functionInvocation

--- Week_4_Practice: Functions/functionInvocation/js/functionInvocation.js
+++ Week_4_Practice: Functions/functionInvocation/js/functionInvocation.js
@@ -3,4 +3,20 @@
  * 5/25/15.
  */
 
-alert("Function Invocation");
+//alert("Function Invocation");
+
+function outptMsg(){
+ console.log("Hello World");
+ }
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea();
+
