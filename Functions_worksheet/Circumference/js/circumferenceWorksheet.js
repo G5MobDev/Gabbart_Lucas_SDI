1abb69452253625146fb1b7a56817d0a23bb4a61
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-26 00:45:09 -0500

    circumferenceWorksheet final code
    Functions Worksheet: Circumference Calculator final code

--- Functions_worksheet/Circumference/js/circumferenceWorksheet.js
+++ Functions_worksheet/Circumference/js/circumferenceWorksheet.js
@@ -5,3 +5,18 @@
  */
 //alert("Circumference Worksheet");
 
+
+
+
+function circle(r, p, x){
+	circ = r * p * x;
+	return circ.toFixed(2);
+}
+var radius = 10;
+var pi = 3.14;
+var multiplier = 2;
+var circumference = circle(radius, pi, multiplier);
+
+circle(radius, pi, multiplier);
+
+console.log(circumference);
