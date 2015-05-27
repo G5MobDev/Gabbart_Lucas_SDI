e00b714d347e217646a5ed443536383677b49507
Lucas Gabbart <lgabbart@fullsail.edu>
2015-05-27 01:12:16 -0500

    created directory for Gabbart_Lucas_Functions_Assignment
    Created directory for Gabbart_Lucas_Functions_Assignment.  Created
    bmiCalc.html and bmiCalc.js files and tested link between html5 and js
    files.
    Commented explanation of what this code is supposed to do, what the
    functions should calculate, and what the functions should return

---
+++ Gabbart_Lucas_Functions_Assignment/js/bmiCalc.js
@@ -0,0 +1,9 @@
+/**Gabbart_Lucas_Functions_Assignment
+ * Body Mass Index(BMI) Calculator
+ * Lucas Gabbart
+ * 5/27/15.
+ */
+//This batch of code will calculate the body mass index and basal metabolic rate of a person based on prompted user input values that will be passed to a series of functions by way of arguments and parameters.  It will return the calculation results to defined result variables and print these values to the console within messages that explain what each values means.
+
+//alert("BMI Calculator");
+
